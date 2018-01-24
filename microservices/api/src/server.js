a0207cde9dd74cc64c6ce65121eab8ab9c2a8e9b
manthan55 <manthanpatidar@gmail.com>
2018-01-24 19:57:59 +0530

    test 1

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -24,6 +24,6 @@ app.get('/r', function (req, res) {
 //   });
 // });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
