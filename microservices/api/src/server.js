db43e101bab0792aaeff6fa9e7a621a4f8ab8e78
manthan55 <manthanpatidar@gmail.com>
2018-01-29 22:52:27 +0530

    tst 1

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -3,11 +3,19 @@ var path = require('path');
 var fetchAction =  require('node-fetch');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var isNumber = require('is-number');
 
 var app = express();
 var router = express.Router();
 
+/// Middleware setup ///
 app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//var urlencodedParser = bodyParser.urlencoded({extended:true});
+////////////////////////
+
 
 var server = require('http').Server(app);
 
@@ -19,15 +27,19 @@ app.set('views',path.join(__dirname,'views'));
 var url_signup = "https://auth.burled79.hasura-app.io/v1/signup";
 var url_login = "https://auth.burled79.hasura-app.io/v1/login";
 var url_logout = "https://auth.burled79.hasura-app.io/v1/user/logout";
+var url_query = "https://data.bleed71.hasura-app.io/v1/query";
 ///////////////////////
 
-/// Middleware setup ///
-var urlencodedParser = bodyParser.urlencoded({extended:false});
-////////////////////////
+
 
 app.get('/', function(req, res) {
   //res.send('Hello World');
   res.render('index');
+});
+
+app.get('/APIEP_Test', function(req, res) {
+  //res.send('Hello World');
+  res.render('APIEP_Test');
 });
 
 app.get('/auth/:accessToken', function(req, res) {
@@ -40,16 +52,80 @@ app.get('/logout', function(req, res) {
   //res.send('Hello World');
   //console.log(req.params.accessToken);
   var auth = req.cookies.auth;
+  console.log(auth);
+  res.clearCookie('auth');
   Logout(auth, res);
+});
+
+app.get('/Loggedin', function (req, res) {
+	res.render('Loggedin');
 });
 
 app.get('/ui', function (req, res) {
 	res.render('index2');
 });
 
+//////////////     Send resource files     ////////////////////
 app.get('/FacebookSDK.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'FacebookSDK.js'));
 });
+app.get('/JCode.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'JCode.js'));
+});
+///////////////////////////////////////////////////////////////
+
+
+
+//////////////////     API Endpoints     //////////////////////
+app.post('/APIEP', function(req, res){
+  var like_user_id = req.body.name;
+  var likeby_user_id = req.body.email;
+  if(isNumber(like_user_id) && isNumber(likeby_user_id)){
+    UpdateLikesTable(like_user_id, likeby_user_id, res);
+  } else {
+    res.send("One or more inputs is invalid (Should be numbers)");
+  }
+});
+///////////////////////////////////////////////////////////////
+
+function UpdateLikesTable(like_user_id, likeby_user_id, res){
+  var requestOptions = {
+    "method": "POST",
+    "headers": {
+        "Content-Type": "application/json"
+    }
+  };
+
+  var body = {
+      "type": "insert",
+      "args": {
+          "table": "Likes_SuperLikes",
+          "objects": [
+              {
+                  "User_id": like_user_id,
+                  "Likes_User_id": likeby_user_id
+              }
+          ]
+      }
+  };
+
+  requestOptions.body = JSON.stringify(body);
+
+  fetchAction(url_query, requestOptions)
+  .then(function(response) {
+  	return response.json();
+  })
+  .then(function(result) {
+  	console.log(JSON.stringify(result));
+    res.send("Done");
+  })
+  .catch(function(error) {
+  	console.log('Request Failed:' + error);
+  });
+  res.send("done!");
+}
+
+
 
 // Uncomment to add a new route which returns hello world as a JSON
 // app.get('/json', function(req, res) {
@@ -84,7 +160,6 @@ function SignUp(accessToken, res){
   .catch(function(error) {
   	console.log('Request Failed:' + error);
   });
-  console.log("its over");
   login(accessToken, res);
 }
 
@@ -112,7 +187,9 @@ function login(accessToken, res){
   .then(function(result) {
   	console.log(result);
     res.cookie('auth',result.auth_token);
-    res.render('Loggedin');
+    //res.render('Loggedin');
+    res.redirect('/Loggedin');
+    //res.send("logged In");
   })
   .catch(function(error) {
   	console.log('Request Failed:' + error);
@@ -135,7 +212,7 @@ function Logout(auth, res){
   	return response.json();
   })
   .then(function(result) {
-  	console.log(JSON.stringify(result));
+  	console.log(result);
     res.send("logged out");
   })
   .catch(function(error) {
---
+++ microservices/api/src/views/JCode.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+  window.jQuery ? console.log('jQuery is loaded once again') : console.log('jQuery is not loaded again');
+}
