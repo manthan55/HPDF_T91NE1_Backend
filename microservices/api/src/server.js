45c664b8db51519adb9eec6aaa38b595d1dec488
manthan55 <manthanpatidar@gmail.com>
2018-02-14 19:16:39 +0530

    testing upload 8

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -164,7 +164,7 @@ app.post('/APIEP_PP', upload.any(), function(req, res, next){
   //var imge=fs.readFileSync(req.file.destination+’/’+imgpath);
   var image=fs.readFileSync(req.files[0].destination+'/'+imagePath);
   var imageType = req.files[0].mimetype;
-  console.log(req.body.user_id);
+  console.log(req.files[0].name);
   UploadPP(image, imageType, res);
 });
 
